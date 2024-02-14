ebafbc4f632911d3d3b4e9cf96da0ebc4171adf8
Kaushik Subramaniam Gnanaskandan <kgnanask@amazon.com>
2024-02-14 12:08:43 -0800

    Initial commit

---
+++ generate.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Directory where files will be created
+target_dir="./files"
+mkdir -p "$target_dir"
+
+# Number of files to create
+num_files=120000
+
+# File size in KB
+file_size_kb=1
+
+for i in $(seq 1 $num_files); do
+    # Use /dev/urandom to generate random data and head to limit to 10KB
+    head -c ${file_size_kb}K /dev/urandom > "$target_dir/file_$i.dat"
+done
+
+echo "Finished creating $num_files files of $file_size_kb KB each."
+
