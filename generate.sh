0f22f88de90bbcc4d2ed747bf45a0a3114a9e4cb
AWS Amplify <42283162+aws-aemilia@users.noreply.github.com>
2024-02-14 12:20:20 -0800

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -5,10 +5,10 @@ target_dir="./files"
 mkdir -p "$target_dir"
 
 # Number of files to create
-num_files=120000
+num_files=${NUM_FILES:-120000}
 
 # File size in KB
-file_size_kb=1
+file_size_kb=${FILE_SIZE_IN_KB:-1}
 
 for i in $(seq 1 $num_files); do
     # Use /dev/urandom to generate random data and head to limit to 10KB
